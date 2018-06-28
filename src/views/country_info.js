e79253eb4c1792e26aab88a3090c3097fc45d50a
Luis Tejero <luisfaridta@gmail.com>
2018-06-28 10:04:47 +0100

    displays list of languages

--- src/views/country_info.js
+++ src/views/country_info.js
@@ -12,6 +12,18 @@ CountriesInfoView.prototype.bindEvents = function(){
   });
 };
 
+CountriesInfoView.prototype.createList = function () {
+ return document.createElement('ul');
+};
+
+CountriesInfoView.prototype.populateList = function (list, data) {
+  data.forEach((item) => {
+    const listItem = document.createElement('li');
+    listItem.textContent = item.name;
+    list.appendChild(listItem);
+  });
+};
+
 CountriesInfoView.prototype.render = function(country){
   const infoParagraph = document.createElement('p');
   infoParagraph.textContent = country.name;
@@ -20,6 +32,18 @@ CountriesInfoView.prototype.render = function(country){
   const regionParagraph = document.createElement('p');
   regionParagraph.textContent = country.region;
   this.container.appendChild(regionParagraph);
+  const language = document.createElement('p');
+  language.textContent = "Languages:";
+  this.container.appendChild(language);
+  const borderList = this.createList();
+  this.populateList(borderList, country.languages)
+  this.container.appendChild(borderList);
 };
+
+
+
+
+
+
 
 module.exports = CountriesInfoView;
